581516246b782e07224a1b1d9493ec61931497e6
CrappyDill <mddillon47@gmail.com>
2022-01-03 14:26:12 -0800

    Navbar styling

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,56 +1,72 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./NavBar.css";
 import { Link, NavLink } from "react-router-dom";
 import { Navbar, Nav, NavItem, NavbarBrand } from "reactstrap";
 import UserContext from "./UserContext";
 
 function NavBar({ logout }) {
-  const currUser = false;
+  const currUser = {name: 'test'};
   // const { currUser } = useContext(UserContext);
 
   function loggedIn() {
     return (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/companies">
-            Companies
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/jobs">
-            Jobs
-          </NavLink>
-        </li>
-        <li className="nav-item mr-4">
-          <NavLink className="nav-link" to="/profile">
-            Profile
-          </NavLink>
-        </li>
+      <div className="signedOut">
+        <NavLink className="nav-link" to="/companies">
+          Companies
+        </NavLink>
+
+        <NavLink className="nav-link" to="/jobs">
+          Jobs
+        </NavLink>
+
+        <NavLink className="nav-link" to="/profile">
+          Profile
+        </NavLink>
+
         {/* <li className="nav-item">
           <Link className="nav-link" to="/" onClick={logout}>
             Log out {currUser.first_name || currUser.username}
           </Link>
         </li> */}
-      </ul>
+      </div>
     );
   }
 
   function loggedOut() {
     return (
-      <ul>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup">Sign Up</NavLink>
-        </li>
-      </ul>
+      <div className="signedOut">
+        <NavLink className="nav-link" to="/login">
+          Login
+        </NavLink>
+
+        <NavLink className="nav-link" to="/signup">
+          Sign Up
+        </NavLink>
+      </div>
     );
   }
 
   return (
-    <nav className="navigation">
-      <Link to="/">
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        borderBottom: "1px solid grey",
+        paddingLeft: "1em",
+        paddingTop: ".5em",
+        paddingBottom: ".5em"
+      }}
+    >
+      <Link
+        style={{
+          fontWeight: "bold",
+          textDecoration: "none",
+          color: "black",
+
+          fontSize: 20,
+        }}
+        to="/"
+      >
         Jobly
       </Link>
       {currUser ? loggedIn() : loggedOut()}
